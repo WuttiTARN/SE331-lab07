e1485cf79536b9136f12c25d1219de3d56eb7be2
wuttiTARN <tarn.wuttiporn@gmail.com>
2016-03-28 18:24:18 +0700

    Q.5

--- src/main/java/camt/se331/shoppingcart/repository/ProductRepository.java
+++ src/main/java/camt/se331/shoppingcart/repository/ProductRepository.java
@@ -3,9 +3,13 @@ package camt.se331.shoppingcart.repository;
 import camt.se331.shoppingcart.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by wuttiTARN on 3/27/2016 AD.
  */
 public interface ProductRepository extends JpaRepository <Product, Long>{
+    public List<Product> findByNameLike(String name);
+    public List<Product> findByNameContaining(String name);
 
 }
